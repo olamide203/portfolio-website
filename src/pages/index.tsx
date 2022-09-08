f53e4a17388510052c7ecb06d86fa5dfb6ef1c35
Olamide Atitebi <olamideumarq@gmail.com>
2022-09-08 16:12:45 +0100

    chore: setup tailwindcss and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,8 +23,8 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+              <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2 className='text-blue-600 bg-slate-300 rounded'>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
