3d253f6c396cf700570c372a322cb5bc46f97013
Olamide Atitebi <olamideumarq@gmail.com>
2022-09-19 08:19:25 +0100

    fix: section headings

--- src/components/Skills/Index.tsx
+++ src/components/Skills/Index.tsx
@@ -6,7 +6,7 @@ import Heading from '../Heading';
 const Skills: React.FC = () => {
 	return (
 		<section className='mb-40 grid grid-cols-1 gap-16 xl:mx-20'>
-			<Heading text='Skills' id={2}></Heading>
+			<Heading text='Skills and Technologies' id={2}></Heading>
 			<div className='grid grid-cols-auto-fit-skills gap-4'>
 				{skills.map((skill: SkillProps, id) => (
 					<Skill {...skill} key={id} />
