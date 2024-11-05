f41febcf8e58673d2962b41f91f7e10311fba4b0
olamide <olamideumarq@gmail.com>
2024-11-05 16:04:17 +0100

    feat: upgrade to NextJS 15

---
+++ next.config.ts
@@ -0,0 +1,13 @@
+import { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      "raw.githubusercontent.com",
+      "www.vectorlogo.zone",
+      "www.gitsocial.net",
+    ],
+  },
+};
+
+export default nextConfig;
